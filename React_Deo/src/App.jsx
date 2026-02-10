7bc9cbc8a7ee55de159a3d43e28072371f807c00
SkHussainnn <skiftekhar92@gmail.com>
2026-02-10 14:28:37 +0530

    first commit

---
+++ React_Deo/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import logo from './assets/Limage.png';
+export default function App() {
+  return (
+    <>
+    <div className="d">
+      <h1 id="h">This is my first react app</h1>
+      <img src={logo} height={100} width={250}></img>
+      </div>
+    </>
+  )
+}
---
+++ React_Deo/src/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+export default function Home() {
+    return (
+        <>
+        <div className="d">
+            <h1>This is my Home first page</h1>
+            <p>Programming language</p>
+            <ol >
+                <li>C</li><li>C++</li><li>Jave</li><li>Python</li><li>SQL</li>
+            </ol>
+        </div>     
+        </>
+    )
+}
