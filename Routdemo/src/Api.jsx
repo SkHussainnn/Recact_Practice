c309e8c7d1dfe06e1680e5fbd5b063644f73615b
SkHussainnn <skiftekhar92@gmail.com>
2026-03-28 15:51:13 +0530

    post put delete

--- Routdemo/src/Api.jsx
+++ Routdemo/src/Api.jsx
@@ -1,43 +1,186 @@
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
 export default function API() {
-const [data, SetData]=useState([])
-let api=()=>{
-    axios.get("http://localhost:8080/detail")
-        .then(res=>{
-            console.log(res.data)
-            SetData(res.data)
-        })
+
+const [data, SetData] = useState([]);
+const [id, SetId] = useState("");
+const [unm, SetUName] = useState("");
+const [ucity, SetUCity] = useState("");
+const [umob, SetUMob] = useState("");
+const [nm, SetName] = useState("");
+const [city, SetCity] = useState("");
+const [mob, SetMob] = useState("");
+//save
+const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+let hndlnm = (e) => SetName(e.target.value);
+let hndlcity = (e) => SetCity(e.target.value);
+let hndlmob = (e) => SetMob(e.target.value);
+
+//update
+const [ushow, SetUShow] = useState(false);
+const uhandleClose = () => SetUShow(false);
+const uhandleShow = () => SetUShow(true);
+let uhndlnm = (e) => SetUName(e.target.value);
+let uhndlcity = (e) => SetUCity(e.target.value);
+let uhndlmob = (e) => SetUMob(e.target.value);
+
+
+
+let getdata=(id,name,city,mob)=>{
+  SetId(id)
+  SetUName(name)
+  SetUCity(city)
+  SetUMob(mob)
+  uhandleShow()
 }
-//onload
-  useEffect(() => {
+
+// API call
+let api = () => {
+  axios.get("http://localhost:8080/stud")
+    .then(res => {
+      SetData(res.data)
+    })
+}
+
+
+// onload
+useEffect(() => {
   api()
 }, [])
-    return (
-        <>
-            <table class="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">Acc_no</th>
-            <th scope="col">Name</th>
-            <th scope="col">Rd_amt</th>
-            <th scope="col">Rd_date</th>
+
+// add student
+let addstud = () => {
+  //alert(nm + " " + city + " " + mob)
+  handleClose()
+  const dt={
+    name: nm,
+    city: city,
+    mob: mob
+  }
+  axios.post("http://localhost:8080/save", dt)
+    .then(() => {
+      //console.log(res.data);
+      alert("Student added successfully")
+      api()
+      setShow(false)
+    })
+}
+
+
+let updtstud = () => {
+  //alert(nm + " " + city + " " + mob)
+  uhandleClose()
+  const dt={
+    id:id,
+    name: unm,
+    city: ucity,
+    mob: umob
+  }
+  axios.put("http://localhost:8080/updt", dt)
+    .then(() => {
+      //console.log(res.data);
+      alert("Update successfully")
+      api()
+      SetUShow(false)
+    })
+    .catch(err => console.log("Update Error: ", err));
+}
+
+let del=(id)=>{
+  alert(id)
+  axios.delete("http://localhost:8080/del/"+id)
+  .then(()=>{
+   alert("Deleted success.....")
+   api()
+   setShow(false)
+  })
+}
+
+return (
+  <>
+    <Button variant="primary" onClick={handleShow}>
+      Add Student
+    </Button>
+    {/*save*/}
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add New Student</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form.Control type="text" onChange={hndlnm} placeholder="Enter name" />
+        <br />
+        <Form.Control type="text" onChange={hndlcity} placeholder="Enter city" />
+        <br />
+        <Form.Control type="number" onChange={hndlmob} placeholder="Enter mobile" />
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={addstud}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+
+    {/*Update*/}
+    <Modal show={ushow} onHide={uhandleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Update Student</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form.Control type="text" value={unm} onChange={uhndlnm} placeholder="Enter name" />
+        <br />
+        <Form.Control type="text" value={ucity} onChange={uhndlcity} placeholder="Enter city" />
+        <br />
+        <Form.Control type="number" value={umob} onChange={uhndlmob} placeholder="Enter mobile" />
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={uhandleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={updtstud} >
+          Update Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+
+
+
+    <table className="table table-dark">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>City</th>
+          <th>Mobile</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.city}</td>
+            <td>{item.mob}</td>
+            <td><Button variant="danger" onClick={() => del(item.id)}>Delete</Button>
+            <Button variant="warning" onClick={() => getdata(item.id, item.name,item.city,item.mob)}>Update</Button></td>
           </tr>
-        </thead>
-        <tbody>
-        {data.map((item)=>{
-          return(
-            <tr>
-              <td>{item.acc_no}</td>
-              <td>{item.name}</td>
-              <td>{item.rd_amt}</td>
-              <td>{item.rd_date}</td>
-            </tr>
-          )
-        })}
-        </tbody>
-      </table>
-        </>
-    );
+        ))}
+      </tbody>
+    </table>
+  </>
+);
 }
